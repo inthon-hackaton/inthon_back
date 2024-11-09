030a01c43354ef1bd04cdf2cfee138a6d1178d9c
soonduck <nederileni99heits@gmail.com>
2024-11-09 19:12:14 +0900

    [revise] picture upload to S3

--- inthon3/services/picture_service.py
+++ inthon3/services/picture_service.py
@@ -1,25 +1,37 @@
-from fastapi import Depends, UploadFile
-from sqlalchemy.orm import Session
+from fastapi import Depends, UploadFile, HTTPException
 from repository.picture_repository import PictureRepository
 from schemas.picture_schema import PictureOutput
-import shutil
+import boto3
 import uuid
 import os
+from dotenv import load_dotenv
 
-UPLOAD_DIRECTORY = "uploads/pictures"
-os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
+load_dotenv()
+
+# AWS 환경 변수
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+S3_BUCKET_NAME = "leaf-bucket"
 
 class PictureService:
     def __init__(self, repository: PictureRepository = Depends()) -> None:
         self.repository = repository
+        self.s3 = boto3.client(
+            's3',
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+        )
 
     def upload_picture(self, file: UploadFile) -> PictureOutput:
         unique_filename = f"{uuid.uuid4()}_{file.filename}"
-        file_path = os.path.join(UPLOAD_DIRECTORY, unique_filename)
+        s3_key = f"uploads/pictures/{unique_filename}"
 
-        with open(file_path, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer) # 파일 저장
+        try:
+            self.s3.upload_fileobj(file.file, S3_BUCKET_NAME, s3_key)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail="Failed to upload file to S3")
 
-        file_url = f"/{file_path.replace(os.sep, '/')}"  # 경로 구분자를 '/'로 변경
+
+        file_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{s3_key}"
 
         return self.repository.create_picture(picture_link=file_url)
