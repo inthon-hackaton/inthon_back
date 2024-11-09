4ca48b5e8fc1ca844a59f514b7cf0d13e1389e15
soonduck <nederileni99heits@gmail.com>
2024-11-09 23:23:19 +0900

    [feat] google oauth verify token

--- inthon3/models/leaf_models.py
+++ inthon3/models/leaf_models.py
@@ -4,22 +4,14 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql.functions import current_timestamp
 from config.database import Base
 
-class Leaf_User(Base):
-    __tablename__ = "leaf_user"
-
-    user_id = Column(Integer, primary_key = True, index = True)
-    nickname = Column(String, unique = True)
-
 class User(Base):
     __tablename__ = "user"
 
-    id = Column(String, primary_key=True, index = True)
-    pw = Column(String)
-    email = Column(String)
-    name = Column(String)
+    user_id = Column(Integer, primary_key = True, index = True)
+    oauth_id = Column(String, unique = True)
+    nickname = Column(String)
     description = Column(Text)
-
-    leaf_user_id = Column(Integer, ForeignKey("leaf_user.user_id"))
+    picture_id = Column(Integer, ForeignKey("picture.picture_id"))
 
 class Picture(Base):
     __tablename__ = "picture"
@@ -34,7 +26,7 @@ class Draft(Base):
     description = Column(Text)
     created_at = Column(DateTime, default=current_timestamp())
 
-    user_id = Column(Integer, ForeignKey("leaf_user.user_id"))
+    user_id = Column(Integer, ForeignKey("user.user_id"))
     picture_id = Column(Integer, ForeignKey("picture.picture_id"))
 
 class Piece(Base):
@@ -45,7 +37,7 @@ class Piece(Base):
     created_at = Column(DateTime, default=current_timestamp())
     description = Column(Text)
 
-    user_id = Column(Integer, ForeignKey("leaf_user.user_id"))
+    user_id = Column(Integer, ForeignKey("user.user_id"))
     draft_id = Column(Integer, ForeignKey("draft.draft_id"))
     picture_id = Column(Integer, ForeignKey("picture.picture_id"))
 
@@ -54,7 +46,7 @@ class Completion(Base):
 
     completion_id = Column(Integer, primary_key=True, index=True)
 
-    user_id = Column(Integer, ForeignKey("leaf_user.user_id"))
+    user_id = Column(Integer, ForeignKey("user.user_id"))
     created_at = Column(DateTime, default=current_timestamp())
 
 class Includes(Base):
@@ -70,7 +62,7 @@ class Includes(Base):
 class Likes(Base):
     __tablename__ = "likes"
 
-    user_id = Column(Integer, ForeignKey("leaf_user.user_id"))
+    user_id = Column(Integer, ForeignKey("user.user_id"))
     piece_id = Column(Integer, ForeignKey("piece.piece_id"))
 
     __table_args__ = (
--- inthon3/routers/api.py
+++ inthon3/routers/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from typing import Optional
-from routers.v1 import picture
+from routers.v1 import picture, auth
 
 router = APIRouter(
     prefix="/api/v1",
@@ -9,6 +9,7 @@ router = APIRouter(
 )
 
 router.include_router(picture.router)
+router.include_router(auth.router)
 
 @router.get("/")
 async def read_root():
