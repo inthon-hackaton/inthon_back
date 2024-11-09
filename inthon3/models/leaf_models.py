842f647ab25cf0de2bc51f6d47350354fe480a96
soonduck <nederileni99heits@gmail.com>
2024-11-09 17:41:58 +0900

    [revise] revise models

--- inthon3/models/leaf_models.py
+++ inthon3/models/leaf_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column,String, Integer, Boolean, ForeignKey, Text, DateTime, URL
+from sqlalchemy import Column,String, Integer, Boolean, ForeignKey, Text, DateTime, PrimaryKeyConstraint
 from pydantic import BaseModel
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.functions import current_timestamp
@@ -25,22 +25,24 @@ class Picture(Base):
     __tablename__ = "picture"
 
     picture_id = Column(Integer, primary_key=True, index=True)
-    picture_link = Column(URL)
+    picture_link = Column(String(2048))
 
 class Draft(Base):
     __tablename__ = "draft"
 
     draft_id = Column(Integer, primary_key= True, index = True)
     description = Column(Text)
-    created_at = Column(DateTime)
+    created_at = Column(DateTime, default=current_timestamp())
 
     user_id = Column(Integer, ForeignKey("leaf_user.user_id"))
     picture_id = Column(Integer, ForeignKey("picture.picture_id"))
 
 class Piece(Base):
+    __tablename__ = "piece"
+
     piece_id = Column(Integer, primary_key=True, index=True)
     piece_number = Column(Integer)
-    created_at = Column(DateTime)
+    created_at = Column(DateTime, default=current_timestamp())
     description = Column(Text)
 
     user_id = Column(Integer, ForeignKey("leaf_user.user_id"))
@@ -48,17 +50,31 @@ class Piece(Base):
     picture_id = Column(Integer, ForeignKey("picture.picture_id"))
 
 class Completion(Base):
+    __tablename__ = "completion"
+
     completion_id = Column(Integer, primary_key=True, index=True)
 
     user_id = Column(Integer, ForeignKey("leaf_user.user_id"))
     picture_id = Column(Integer, ForeignKey("picture.picture_id"))
 
 class Includes(Base):
+    __tablename__ = "includes"
+
     piece_id = Column(Integer, ForeignKey("piece.piece_id"))
     completion_id = Column(Integer, ForeignKey("completion.completion_id"))
 
+    __table_args__ = (
+        PrimaryKeyConstraint("piece_id", "completion_id"),
+    )
+
 class Likes(Base):
+    __tablename__ = "likes"
+
     user_id = Column(Integer, ForeignKey("leaf_user.user_id"))
     piece_id = Column(Integer, ForeignKey("piece.piece_id"))
+
+    __table_args__ = (
+        PrimaryKeyConstraint("user_id", "piece_id"),
+    )
 
 
