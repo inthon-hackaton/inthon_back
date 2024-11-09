6dd72931aa11492a416988f61f63d07557f71cb1
Euizzang8001 <a73875323@gmail.com>
2024-11-10 07:48:08 +0900

    [delete] no used files

--- inthon3/services/services_sample.py
+++
@@ -1,6 +0,0 @@
-from fastapi import Depends, HTTPException, Response
-from repository.repository_sample import SampleRepository
-
-class SampleService():
-    def __init__(self, repository: SampleRepository = Depends()) -> None:
-        self.repository = repository
